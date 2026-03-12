ee06edb86e0f53542bf64a2caad70a3e15ea538f
Swayam-Alhat <swayamalhat@gmail.com>
2026-03-12 08:24:45 +0530

    added point about EDA

--- Linear-Regression/multiple_L_R.py
+++ Linear-Regression/multiple_L_R.py
@@ -12,3 +12,11 @@ df = df.sample(frac=1, random_state=42).reset_index(drop=True)
 # check if null value is present
 is_null = df.isnull().any().any()
 print(is_null) # False
+
+# check if data is linear (i.e features have linear relationship with target)
+features = ["area","bedrooms"]
+for feature in features:
+    plt.scatter(df[feature],df["price"])
+    plt.xlabel(feature)
+    plt.ylabel("price")
+    plt.show()
